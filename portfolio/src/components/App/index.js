9aff9513664cb58dbfca6e53e6439c50a4f1e917
stacyjubiniaux <stacy.jubiniaux@gmail.com>
2021-04-26 21:28:59 +0200

    start of project with the header

---
+++ portfolio/src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import : npm
+import React from 'react';
+
+// == Import : local
+import Header from '../Header';
+
+// == Import : style
+import './style.scss';
+
+const App = () => (
+  <div className="app">
+    <Header />
+  </div>
+);
+
+export default App;
---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// == Import : local
+import logo from '../../assets/images/logo.png';
+import './style.scss';
+
+const Header = () => (
+    <div className="header">
+      <div>
+        <img src={logo} className='header-logo' />
+      </div>
+      {/* <ul className='header-list'>
+        <li className='header-item'><a className='header-link' href='#'>About me</a></li>
+        <li className='header-item'><a className='header-link' href='#'>Projets</a></li>
+        <li className='header-item'><a className='header-link' href='#'>Contacts</a></li>
+      </ul> */}
+      <section className='header-list'>
+        <a>
+          <span className="text">About me</span>
+          <span className="line -right"></span>
+          <span className="line -top"></span>
+          <span className="line -left"></span>
+          <span className="line -bottom"></span>
+        </a>
+        <a>
+          <span className="text">Projects</span>
+          <span className="line -right"></span>
+          <span className="line -top"></span>
+          <span className="line -left"></span>
+          <span className="line -bottom"></span>
+        </a>
+        <a>
+          <span className="text">Contacts</span>
+          <span className="line -right"></span>
+          <span className="line -top"></span>
+          <span className="line -left"></span>
+          <span className="line -bottom"></span>
+        </a>
+      </section>
+    </div>
+  );
+
+export default Header;
